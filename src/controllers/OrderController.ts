04d7f66c712c63c847c45f01077955a992e2aab6
WandersonJunio <wanderson.junioantunes@outlook.com>
2021-11-04 22:21:37 -0300

    Criação de config | Preparação do projeto para typeScript | Criação de controller

---
+++ src/controllers/OrderController.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+class OrderController {
+  public async create(req: Request, res: Response, next: NextFunction) {
+    
+  }
+}
+
+export default new OrderController();
